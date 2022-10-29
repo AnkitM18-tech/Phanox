81dbf7814a36952802b847cf682feb5a545fa7f7
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2022-10-29 16:57:25 +0530

    Layout Component

--- phanox/components/Layout.jsx
+++ phanox/components/Layout.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Head from "next/head";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
-const Layout = () => {
-  return <div>Layout</div>;
+const Layout = ({ children }) => {
+  return (
+    <div className="layout">
+      <Head>
+        <title>Headphone Store</title>e
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">{children}</main>
+      <Footer />
+    </div>
+  );
 };
 
 export default Layout;
