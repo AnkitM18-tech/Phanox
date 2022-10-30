f60b4de9717ac38b7da54863a890c72990b41424
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2022-10-31 03:06:48 +0530

    minor fix

--- phanox/pages/product/[slug].js
+++ phanox/pages/product/[slug].js
@@ -111,10 +111,12 @@ export const getStaticPaths = async () => {
 };
 
 export const getStaticProps = async ({ params: { slug } }) => {
-  const query = `*[_type == 'product' && slug.current == "${slug}"][0]`;
+  const query = `*[_type == 'product' && slug.current == '${slug}'][0]`;
   const productsQuery = "*[_type == 'product']";
   const product = await client.fetch(query);
   const products = await client.fetch(productsQuery);
+
+  console.log(product);
 
   return {
     props: { product, products },
